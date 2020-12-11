9683902a6b4ed4d0fdbe5823a736b90438608cff
sfave <sebpfave@gmail.com>
2020-12-10 20:55:42 -0500

    IOControl refactoring

--- io_control.py
+++ io_control.py
@@ -1,6 +1,10 @@
 import pandas as pd
 
 
+# Constants
+file_encoding = "utf-8"
+
+
 # Functions
 def dataframe_to_dict(dataframe):
     return dataframe.to_dict(orient="records")
@@ -16,12 +20,12 @@ class IOControl():
         self.lang_cards_learn = f"data/{lang_from.lower()}_words_to_learn.csv"
 
     def get_all_cards(self):
-        with open(self.lang_cards_all, mode="r", encoding="utf-8") as data_file:
+        with open(self.lang_cards_all, mode="r", encoding=file_encoding) as data_file:
             return pd.read_csv(data_file)
 
     def get_cards(self):
         try:
-            with open(self.lang_cards_learn, mode="r", encoding="utf-8") as data_file:
+            with open(self.lang_cards_learn, mode="r", encoding=file_encoding) as data_file:
                 cards_data = pd.read_csv(data_file)
         except FileNotFoundError:
             cards_data = self.get_all_cards()
@@ -30,5 +34,6 @@ class IOControl():
 
     def write_cards(self, words_to_learn):
         data_words_to_learn = pd.DataFrame(words_to_learn)
-        with open(self.lang_cards_learn, mode="w", newline="", encoding="utf-8") as write_file:
+        with open(
+                self.lang_cards_learn, mode="w", newline="", encoding=file_encoding) as write_file:
             data_words_to_learn.to_csv(write_file, index=False)
